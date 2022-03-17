08e6978458c6030a6639d6449f36d3e2647db263
Kaynat Zehra <kaynatzehra23@gmail.com>
2022-03-17 19:01:29 +0530

    Added: Address Book program using IO and Address book txt file created

---
+++ src/main/java/com/bridgelabz/AddressBook/AddressBook.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.AddressBook;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddressBook {
+    public static Map<String, Contact> book = new HashMap<>();
+
+}
